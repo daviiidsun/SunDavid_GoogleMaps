b0763c9227ffef09eb2adc6f8e41b12f064a6bd9
sund9363 <daviiidsun@gmail.com>
2018-06-04 09:24:36 -0700

    Fixed view

--- app/src/main/java/com/example/sund9363/mymapsapp_p1/MapsActivity.java
+++ app/src/main/java/com/example/sund9363/mymapsapp_p1/MapsActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -87,8 +88,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     // Add a View button and method to switch between satellite and map view
-    public void changeView(){
-        mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
+    public void changeView(View view){
+        if (mMap.getMapType() != mMap.MAP_TYPE_SATELLITE){
+            mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
+        } else {
+            mMap.setMapType(mMap.MAP_TYPE_NORMAL);
+        }
     }
 
     //onSearch()
